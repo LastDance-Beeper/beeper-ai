1770dcde3ef6897de1ce1cc1ece7c6ab6afc33d1
[GitHub <[GitHub>
2024-08-14 02:29:47 +0900

    클로버 speech 완료

---
+++ clover_speech.py
@@ -0,0 +1,95 @@
+import requests
+import json
+
+
+class ClovaSpeechClient:
+    # Clova Speech invoke URL
+    invoke_url = 'https://clovaspeech-gw.ncloud.com/external/v1/8762/43f0e078ac850011acc6d07dadee6b472ace926950c93c1edca6bc69ce9b6213'
+    # Clova Speech secret key
+    secret = '1655e22448bb424ebd1eb576c9c1899d'
+
+    def req_url(self, url, completion, callback=None, userdata=None, forbiddens=None, boostings=None, wordAlignment=True, fullText=True, diarization=None, sed=None):
+        request_body = {
+            'url': url,
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+            'sed': sed,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'Content-Type': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        return requests.post(headers=headers,
+                             url=self.invoke_url + '/recognizer/url',
+                             data=json.dumps(request_body).encode('UTF-8'))
+
+    def req_object_storage(self, data_key, completion, callback=None, userdata=None, forbiddens=None, boostings=None,
+                           wordAlignment=True, fullText=True, diarization=None, sed=None):
+        request_body = {
+            'dataKey': data_key,
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+            'sed': sed,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'Content-Type': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        return requests.post(headers=headers,
+                             url=self.invoke_url + '/recognizer/object-storage',
+                             data=json.dumps(request_body).encode('UTF-8'))
+
+    def req_upload(self, file, completion, callback=None, userdata=None, forbiddens=None, boostings=None,
+                   wordAlignment=True, fullText=True, diarization=None, sed=None):
+        request_body = {
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+            'sed': sed,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        print(json.dumps(request_body, ensure_ascii=False).encode('UTF-8'))
+        files = {
+            'media': open(file, 'rb'),
+            'params': (None, json.dumps(request_body, ensure_ascii=False).encode('UTF-8'), 'application/json')
+        }
+        response = requests.post(headers=headers, url=self.invoke_url + '/recognizer/upload', files=files)
+        return response
+
+if __name__ == '__main__':
+    res = ClovaSpeechClient().req_upload(file='test_file.m4a', completion='sync')
+    data = res.json()
+    print(data["text"])
+    json_string = {
+        'content': data["text"]
+    }
+    headers = {
+        'Accept': 'application/json;UTF-8',
+        'Content-Type': 'application/json;UTF-8'
+    }
+    response = requests.post(headers=headers, url="127.0.0.1:8080/text", files = json_string)
